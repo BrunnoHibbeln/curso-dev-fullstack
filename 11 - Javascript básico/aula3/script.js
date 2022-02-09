04cfd3765676f63ce698d87b6c1bc40a54c71977
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-09 14:59:04 -0300

    aulas 3 e 4

---
+++ 11 - Javascript básico/aula3/script.js
@@ -0,0 +1,36 @@
+
+// comentário de uma linha
+
+/*
+    comentário de multiplas linhas
+*/
+
+/*
+
+    Um programa de computador é um lista de "instruções" a serem "executadas" por um computador.
+
+    Um programa JavaScript é uma lista de instruções de programação.
+
+    Valores, operadores, expressões, palavras-chave e comentários.
+
+    PALAVRAS-CHAVE:
+
+    break
+    continue
+    debugger
+    do .. while
+    for
+    function
+    if ...
+    return
+    switch
+    try ...
+    var
+    let
+    const
+
+*/
+
+console.log('Hello word')
+
+document.querySelector('#elemento').src = 'imagem.jpg'
