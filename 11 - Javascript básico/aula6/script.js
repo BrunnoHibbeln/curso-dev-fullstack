0fa5befebb03629fc676124395f1e5dd73f1a90a
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-15 23:51:14 -0300

    aula6

---
+++ 11 - Javascript básico/aula6/script.js
@@ -0,0 +1,28 @@
+/* 
+
+String
+Number
+Boolean
+Array
+Object
+Undefined
+null
+
+*/
+
+//String
+let nome = 'Brunno'
+
+//String concatenada
+let sobrenome = 'Hibbeln'
+
+    //Forma antiga
+    console.log(nome + ' ' + sobrenome)
+
+    //Forma atual
+    console.log(`${nome} ${sobrenome}`)
+
+//Number
+let idade = 35
+console.log(35 + 10)
+    
