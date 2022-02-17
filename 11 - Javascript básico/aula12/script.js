bc9647e24d9dab9f358c36c593d663f9557c13d8
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-17 19:44:21 -0300

    laço de repetição - FOR

---
+++ 11 - Javascript básico/aula12/script.js
@@ -0,0 +1,16 @@
+let opcoes
+
+for(let contador = 2021; contador >= 1900; contador--) {
+    opcoes += `<option>${contador}</option>`
+}
+
+document.querySelector('#ano').innerHTML = opcoes
+
+let clientes = ['Thiado', 'Raphael', 'Brunno', 'Suzy']
+let lista = ''
+
+for(contador = 0; contador <= clientes.length - 1; contador++) {
+    lista += `<li>${clientes[contador]} ${contador}</li>`
+}
+
+document.querySelector('#listaClientes').innerHTML = lista
