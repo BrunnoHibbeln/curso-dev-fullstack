109b8108bce5bf70142d380c3647f962223e5eec
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-16 21:32:36 -0300

    Operadores comparativos

---
+++ 11 - Javascript básico/aula8/script.js
@@ -0,0 +1,40 @@
+
+// == Valores iguais
+let a = 10
+let b = '10'
+console.log(a == b)
+
+// === Valores e tipos iguais
+let c = 10
+let d = '10'
+console.log(c === d)
+
+// !== Valores diferentes
+let e = 10
+let f = '10'
+console.log(e != f)
+
+// !== Valores e tipos diferentes
+let g = 10
+let h = '10'
+console.log(g !== h)
+
+// > Valores maiores
+let i = 10
+let j = 15
+console.log(i > j)
+
+// < Valores menores
+let k = 10
+let l = 15
+console.log(k < l)
+
+// >= Valores maiores ou iguais
+let m = 10
+let n = 10
+console.log(m >= n)
+
+// <= Valores menores ou iguais
+let o = 12
+let p = 10
+console.log(o <= p)
