d9d98f79454b6f4843cb3f9a015794fdcc30a3c7
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-16 21:12:40 -0300

    operadores aritimeticos e de atribuicao

---
+++ 11 - Javascript básico/aula7/script.js
@@ -0,0 +1,61 @@
+/* 
+
+adição +
+subtração -
+multiplicação *
+divisão /
+incremento ++
+decremento --
+atribuição += e -=
+
+*/
+
+/* 
+    Caso tente somar um número que não esteja definido ou null, retornará:
+
+        NaN = Not a Number
+*/
+
+//Adição
+let a = 10
+let b = 5
+let c = a + b
+console.log(c)
+
+//Subtração
+let d = 10
+let e = 15
+let f = d - e
+console.log(f)
+
+//Multiplicação
+let g = 10
+let h = 5
+let i = g * h
+console.log(i)
+
+//Divisão
+let j = 10
+let k = 2
+let l = j / k
+console.log(l)
+
+//incremento
+let m = 10
+m++
+console.log(m)
+
+//decremento
+let n = 10
+n--
+console.log(n)
+
+//atribuição +
+let o = 12
+o += 25 //O += significa o = o + 25, ou seja, a váriavel igual a ela mesma mais 25.
+console.log(o)
+
+//atribuição -
+let p = 12
+p -= 25 
+console.log(p)
