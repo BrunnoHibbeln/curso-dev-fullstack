d14d80eacd914a215c790684ce4a7aeb781419e5
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-21 16:36:16 -0300

    While

---
+++ 11 - Javascript básico/aula14/script.js
@@ -0,0 +1,9 @@
+
+let contador = 0
+
+//Como o nome já diz, na estrutura "while" devemos por uma condição, que enquanto ela seja verdadeira, o comando será rodado
+while (contador < 10) {
+    console.log('Hello world!')
+
+    contador++
+}
