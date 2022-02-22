2eae990642722c11264cb779bd1cfd5165a8e186
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-02-22 10:11:52 -0300

    exercicio1

---
+++ 11 - Javascript básico/exercicio1/script.js
@@ -0,0 +1,9 @@
+nome = window.prompt('Digite seu nome')
+
+idade = window.prompt('Digite sua idade')
+
+if (idade > 17) {
+    alert(`Olá ${nome}, você é MAIOR de idade`)
+} else {
+    alert(`Olá ${nome}, você é MENOR de idade`)
+}
