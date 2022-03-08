c99ae4cae74e38fee359d4a1c70b0e380b860084
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-03-07 21:24:10 -0300

    Adicionar tarefas

--- 12 - Javascript Intermediário/To Do List/js/main.js
+++ 12 - Javascript Intermediário/To Do List/js/main.js
@@ -19,6 +19,8 @@ const Main = {
 
         // Aqui também usamos o 'this' na criação de variáveis para que possamos usa-las em outras funções do 'Main'
         this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
     },
 
     // Função referenciar os eventos
@@ -30,6 +32,9 @@ const Main = {
         this.$checkButtons.forEach(function(button){
             button.onclick = self.Events.checkButton_click
         })
+
+        // Colocamos o '.bind(this)' no final para que dentro da função o 'this' se referêncie ao 'Main'. Pois dentro de qualquer evento, o this se referência ao próprio elemento ligado ao evento.
+        this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
     },
 
     // Definição de eventos
@@ -45,10 +50,29 @@ const Main = {
         // É uma boa prática estruturar o if da seguinte maneira:
 
             if(!isDone) { // Verificar primeiro a negação
-                
+
                 return li.classList.add('done') // Adicionar o 'return' para que não sejam executados ambos os comandos ao mesmo tempo
             }
             li.classList.remove('done')
+        },
+
+        inputTask_keypress: function(e){
+            const key = e.key
+            const value = e.target.value
+
+            if(key === 'Enter') {
+                this .$list.innerHTML += `
+                
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">
+                            ${value}
+                        </label>
+                        <button class="remove"></button>    
+                    </li>   
+                `
+                e.target.value = ''
+            }
         }
     }
 }
