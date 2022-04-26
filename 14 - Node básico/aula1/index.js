dff9397a0aa1b74ac43ed99ce888a2cc1d530804
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-04-26 15:30:27 -0300

    hello word

---
+++ 14 - Node básico/aula1/index.js
@@ -0,0 +1,5 @@
+console.log('Hello world')
+
+// Para executar o programa devemos entrar na pasta do index.js pelo terminal e digitar 'node index.js'
+
+console.log(document, window) // No node não temos os elementos document e window do DOM, mas temos um ambiente javascript que funcionará quase que da mesma forma.
