8d44b54e3e88efb6b345112f25bc4e5d5c305d65
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-04-28 20:48:59 -0300

    criando rotas

--- 15 - Node express/express-server/index.js
+++ 15 - Node express/express-server/index.js
@@ -5,6 +5,18 @@ const express = require('express')
 // Guardamos a aplicação dentro da constânte 'app'
 const app = express()
 
+// Rota padrão
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+// Quando o usuário digitar uma rota que não existe, será exibido essa mensagem
+app.use((req, res) => { // 404 error (not found)
+    res.send('Página não encontrada')
+})
+
+// O código acima é chamado de 'middleware' que pode ser executado entre uma requisição e uma execução de rota
+
 // O processo que estiver executando a aplicação que irá determinar em qual porta o servidor será hospedado
 const port = process.env.PORT || 8080 // Colocamos o '|| 8080' para caso o valor da porta não for informado, será em um valor padrão, no caso '8080'
 
