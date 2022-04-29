df4a46493c014967ba1faf477eae5b8ebbb4639f
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-04-29 13:05:47 -0300

    Servindo arquivos estáticos e públicos

--- 15 - Node express/express-server/index.js
+++ 15 - Node express/express-server/index.js
@@ -2,18 +2,34 @@
 // 'const express' se torna uma função que retorna uma aplicação em 'express'
 const express = require('express')
 
+const path = require('path')
+
 // Guardamos a aplicação dentro da constânte 'app'
 const app = express()
 
-// Rota padrão
+// O 'MVC (Model View Controller)' basicamente, (Model) é tudo o que vai lidar com o banco de dados, (View) tudo o que lida com a camada de visualização, (Controle) gerenciar e manipular todos os dados.
+
+// Definir arquivos estáticos
+
+    // 'path.join' usamos para trazer o nome do diretório e concatenar outro valor
+    app.use(express.static(path.join(__dirname, 'views'))) // Middleware
+
+// Definir arquivos públicos
+
+    const publicFolder = path.join(__dirname, 'public')
+    const expressPublic = express.static(publicFolder)
+    app.use(expressPublic)
+
+// Rotas
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('views/index')
 })
 
 // Quando o usuário digitar uma rota que não existe, será exibido essa mensagem
 app.use((req, res) => { // 404 error (not found)
     res.send('Página não encontrada')
 })
+
 
 // O código acima é chamado de 'middleware' que pode ser executado entre uma requisição e uma execução de rota
 
