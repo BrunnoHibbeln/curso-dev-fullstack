f6538b0a5b509fe038fab5bc7c4b1d88e6e5d275
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-05-26 13:32:34 -0300

    Lidando com formularios

--- 15 - Node express/express-server/index.js
+++ 15 - Node express/express-server/index.js
@@ -2,6 +2,7 @@
 // 'const express' se torna uma função que retorna uma aplicação em 'express'
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 // Guardamos a aplicação dentro da constânte 'app'
 const app = express()
@@ -17,7 +18,6 @@ app.set('view engine', 'ejs') // Logo após isso, devemos alterar a extensão do
                         app.use(express.static(path.join(__dirname, 'views'))) Middleware
 
                     Definir arquivos públicos
-
                     */
 
 const publicFolder = path.join(__dirname, 'public')
@@ -25,6 +25,8 @@ const expressPublic = express.static(publicFolder)
 app.use(expressPublic) 
 // Também podemos fazer dessa forma: app.use(express.static(path.join(__dirname, 'public')))
 
+// Para receber dados de um formulário pelo método POST, devemos configurar o servidor para receber esses dados através de um middleware
+app.use(express.urlencoded({ extented: true })) // Esse middleware nos permite receber dados (de formulários) via POST
 
 // Rotas
 app.get('/', (req, res) => {
@@ -59,6 +61,44 @@ app.get('/posts', (req, res) => {
     })
 })
 
+app.get('/cadastro-posts', (req, res) => {
+    const { cadastro } = req.query
+
+    res.render('cadastro-posts', {
+        title: 'Cadastrar post',
+        cadastrado: cadastro,
+    })
+})
+
+app.post('/salvar-post', (req, res) => {
+    
+    // Como configuramos um middleware, para receber os dados do formulário via POST, agora podemos acessar esses dados que são armazenados no 'req' dentro de um objeto chamado 'body'
+    
+    const { titulo, texto } = req.body
+
+    /* Criando um diretório para salvar os dados dentro do arquivo "posts.json" */
+
+    // Como o método 'readFile' é assíncrono e não vamos precisar utilizar um callback, devemos torna-lo síncrono, para que somente retorne o conteúdo do arquivo. Assim, colocamos o conteúdo dentro da variável 'data'
+    const data = fs.readFileSync('./store/posts.json')
+
+    // Colocamos o valor de 'data' dentro de 'post' convertendo o valor de "string" para "json"
+    const posts = JSON.parse(data)
+
+    posts.push({
+        titulo,
+        texto,
+    })
+        
+    // Convertendo posts novamente em "string"
+    const postsString = JSON.stringify(posts)
+
+    // O primeiro parâmetro é o caminho do arquivo e o segundo é o conteúdo que queremos inserir nesse mesmo arquivo
+    fs.writeFileSync('./store/posts.json', postsString)
+
+    // Quando o formulário for enviado, a página irá para a rota depois será redirecionada novamente para a mesma página. Mas vamos passar um parâmetro pelo url para podermos verificar e exibir uma mensagem de confirmação para o usuário. Passamos o parâmetro após o "?". 
+    res.redirect('/cadastro-posts?cadastro=1')
+})
+
 // Quando o usuário digitar uma rota que não existe, será exibido essa mensagem
 app.use((req, res) => { // 404 error (not found)
     res.send('Página não encontrada')
@@ -71,4 +111,4 @@ app.use((req, res) => { // 404 error (not found)
 const port = process.env.PORT || 8080 // Colocamos o '|| 8080' para caso o valor da porta não for informado, será em um valor padrão, no caso '8080'
 
 // Colocamos a porta onde o servidor será hospedado e logo sem seguida um callback
-app.listen(port, () => console.log(`Server is listening on port 8080`))
+app.listen(port, () => console.log(`Server is listening on port 8080`))   
