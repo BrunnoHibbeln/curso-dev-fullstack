d14a9e5445e49341f9edae22b302fb42146c789e
BrunnoHibbeln <brunnohibbeln10@gmail.com>
2022-06-02 18:04:54 -0300

    Reutilização de componentes

--- 18 - React.js básico/hello-world-react/src/App.js
+++ 18 - React.js básico/hello-world-react/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import Header from './Header'
-import Content from './Content'
+
+import Header from './Header';
+import Content from './Content';
+import Card from './Card';
+import Cards from './Cards';
+
+// Importando imagens
+import reactImg from './imagens/react.png';
+import nextImg from './imagens/next.png';
+import nodeImg from './imagens/node.png';
 
 /* 
     COMPONENTES - Criando e Usando
@@ -28,6 +36,26 @@ function App() {
                 <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
 
                 <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                    
+                <Cards>
+                    <Card
+                        imagem={reactImg}
+                        titulo="React.js"
+                        texto="ctionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem"
+                    />
+
+                    <Card 
+                        imagem={nextImg}
+                        titulo="Next.js"
+                        texto="ctionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem"
+                    />
+
+                    <Card 
+                        imagem={nodeImg}
+                        titulo="Node.js"
+                        texto="ctionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem"
+                    />
+                </Cards>
             </Content>
         </>
     )
