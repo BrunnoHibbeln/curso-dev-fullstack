82481d3ff5ebd7e753476ede95034ffd576b06ad
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-06-13 08:50:12 -0300

    editar clientes

--- 16 - MongoDB MVC/projeto-crud/src/routes/index.js
+++ 16 - MongoDB MVC/projeto-crud/src/routes/index.js
@@ -13,6 +13,12 @@ router.get('/register', CustomersController.index)
 router.post('/register/add', CustomersController.add)
 
 // Listar
-router.get('/list', CustomersController.listUsers)
+router.get('/list', CustomersController.list)
+
+// Editar
+router.get('/edit', CustomersController.formEdit) // Recebe os dados para serem editados
+
+// Como vamos enviar os dados passando um 'id' pelo '/', devemos informar o envio desse parâmetro '/edit/:id'
+router.post('/edit/:id', CustomersController.edit) // Altera os dados no banco
 
 module.exports = router
