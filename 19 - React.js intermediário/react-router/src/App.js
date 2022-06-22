21861e46688d759d22bbab90d78311e3be9d6bb0
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-06-22 13:57:36 -0300

    react router

---
+++ 19 - React.js intermediário/react-router/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+	BrowserRouter as Router, Switch, Route
+} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Users from './pages/Users'
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/users">
+					<Users />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
+	)
+}
+
+export default App
