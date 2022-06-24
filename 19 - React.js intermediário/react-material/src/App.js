fb80f2779fd30f32950950f629453ea713f033cf
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-06-24 08:57:09 -0300

    material design e ui

---
+++ 19 - React.js intermediário/react-material/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// Importar somente o 'react' nas versões > 17 é opicional. Somente se formos usar o 'useState' ou 'useEffect'
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <Button variant="contained" color="secondary">
+      Button
+    </Button>
+  )
+}
+
+export default App
