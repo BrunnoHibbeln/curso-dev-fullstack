9ade7e3dbf6b63b334137f2d3f647ab2aee8778c
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-06-27 21:47:56 -0300

    Grid - my-app

--- 19 - React.js intermediário/my-app/src/components/CustomerCard.js
+++ 19 - React.js intermediário/my-app/src/components/CustomerCard.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import classNames from 'classnames'
 
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardActions from '@material-ui/core/CardActions'
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
+import {
+   Card,
+   CardHeader,
+   CardActions,
+   Avatar,
+   IconButton
+} from '@material-ui/core'
 
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import ShareIcon from '@material-ui/icons/Share'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     maxWidth: 345,
   },
-}));
+}))
 
 const CustomerCard = ({
    name,
    lastname,
    email,
    avatar,
+   className,
 }) => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
+    <Card className={classNames(className, classes.root)}>
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" src={avatar}>
