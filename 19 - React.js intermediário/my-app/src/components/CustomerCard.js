9c665995136d687d17fab271d68ea8d419b4c84e
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-07-07 00:20:18 -0300

    deletar usuário - confirm modal

--- 19 - React.js intermediário/my-app/src/components/CustomerCard.js
+++ 19 - React.js intermediário/my-app/src/components/CustomerCard.js
@@ -1,54 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 
 import {
-   Card,
-   CardHeader,
-   CardActions,
-   Avatar,
-   IconButton
+	Card,
+	CardHeader,
+	CardActions,
+	Avatar,
+	IconButton
 } from '@material-ui/core'
 
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+import ModalConfirm from './ModalConfirm'
 
 const useStyles = makeStyles(() => ({
-  root: {
-    maxWidth: 345,
-  },
+	root: {
+		maxWidth: 345,
+	},
 }))
 
 const CustomerCard = ({
-   name,
-   lastname,
-   email,
-   avatar,
-   className,
+	name,
+	lastname,
+	email,
+	avatar,
+	className,
 }) => {
-  const classes = useStyles();
+	const classes = useStyles()
+	const [openModal, setOpenModal] = useState(false)
 
-  return (
-    <Card className={classNames(className, classes.root)}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" src={avatar}>
-            R
-          </Avatar>
-        }
-        title={`${name} ${lastname}`}
-        subheader={email}
-      />
-      <CardActions disableSpacing>
-        <IconButton aria-label="Editar">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="Remover">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-  )
+	const handleToggleOpenModal = () => {
+		setOpenModal(!openModal)
+	}
+	const handleConfirmModal = () => {
+		alert('ok')
+	}
+	const handleRemoveCustomer = () => {
+		handleToggleOpenModal()
+	}
+
+	return (
+		<>
+			<Card className={classNames(className, classes.root)}>
+				<CardHeader
+					avatar={
+						<Avatar aria-label="recipe" src={avatar}>
+							R
+						</Avatar>
+					}
+					title={`${name} ${lastname}`}
+					subheader={email}
+					/>
+				<CardActions disableSpacing>
+					<IconButton aria-label="Editar">
+						<EditIcon />
+					</IconButton>
+					<IconButton aria-label="Remover" onClick={ handleRemoveCustomer }>
+						<DeleteIcon />
+					</IconButton>
+				</CardActions>
+			</Card>
+			<ModalConfirm
+				open={ openModal }
+				onClose={ handleToggleOpenModal }
+				onConfirm={ handleConfirmModal }
+				title="Do you really want to delete?"
+				message="Once confirmed, it will not be possible to revert."
+			/>
+		</>
+	)
 }
 
 export default CustomerCard
