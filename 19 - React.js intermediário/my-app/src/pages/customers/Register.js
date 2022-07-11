7a376e1f6d951cd8a7a582c69ec36a5a665c6579
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-07-11 10:52:38 -0300

    cadastro - integração com a api

--- 19 - React.js intermediário/my-app/src/pages/customers/Register.js
+++ 19 - React.js intermediário/my-app/src/pages/customers/Register.js
@@ -1,7 +1,10 @@
-import { makeStyles } from '@material-ui/core'
-
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+import { useState } from 'react'
+import axios from 'axios'
+import {
+   makeStyles,
+   TextField,
+   Button,
+} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
    wrapper: {
@@ -11,17 +14,93 @@ const useStyles = makeStyles((theme) => ({
 
 const Register = () => {
    const classes = useStyles()
+
+   const [form, setForm] = useState({
+      name: {
+         value: '',
+         error: false,
+      },
+      job: {
+         value: '',
+         error: false,
+      },
+   })
+
+   const handleInputChange = (e) => {
+      const { name, value } = e.target
+
+      setForm({
+         ...form,
+         [name]: {
+            value
+         },
+      })
+   }
+   const handleRegisterButton = () => {
+      let hasError = false
+      let newFormState = {
+         ...form,
+      }
+
+      if (!form.name.value) {
+         hasError = true
+
+         newFormState.name = {
+            value: form.name.value,
+            error: true,
+            helperText: 'Incorrect entry',
+         }
+      }
+      if (!form.job.value) {
+         hasError = true
+
+         newFormState.job = {
+            value: form.job.value,
+            error: true,
+            helperText: 'Incorrect entry',
+         }
+      }
+
+      if (hasError) {
+         return setForm(newFormState)
+      }
+
+      axios.post('https://reqres.in/api/users', {
+         name: form.name.value,
+         job: form.job.value,
+      }).then(response => {
+         console.log('ok', response)
+      })
+   }
    
    return (
       <>
          <div className={ classes.wrapper }>
-            <TextField label="Type your name" variant="outlined"></TextField>
+            <TextField
+               error={form.name.error}
+               helperText={form.name.error ? form.name.helperText : ''}
+               label="Type your name" 
+               name="name"
+               value={form.name.value} 
+               onChange={handleInputChange} 
+               variant="outlined"
+            />
          </div>
          <div className={ classes.wrapper }>
-            <TextField label="Enter your position" variant="outlined"></TextField>
+            <TextField 
+               error={form.job.error}
+               helperText={form.job.error ? form.job.helperText : ''}
+               label="Enter your position" 
+               name="job"
+               value={form.job.value} 
+               onChange={handleInputChange} 
+               variant="outlined"
+            />
          </div>
          <div className={ classes.wrapper }>
-            <Button variant="contained" color="primary">Confirm</Button>
+            <Button variant="contained" color="primary" onClick={handleRegisterButton}>
+               Confirm
+            </Button>
          </div>
       </>
    )
