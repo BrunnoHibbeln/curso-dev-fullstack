daa03245a02bb0a9a6d9b90cf13a56ba1a351479
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2022-08-10 11:11:25 -0300

    adicionando metodo PUT

--- 17 - API RESTful/api-restful/src/controllers/products.js
+++ 17 - API RESTful/api-restful/src/controllers/products.js
@@ -26,8 +26,31 @@ async function post(req, res) {
       message: 'Success!'
    })
 }
+async function put(req, res) {
+   const { id } = req.params
+
+   /* 
+      const product = await ProductsModel.findById({ _id: id })
+      await product.updateOne(req.body)
+      
+      res.send({
+         message: 'Success',
+         product,
+      })
+   */
+      // Dessa forma acima funciona perfeitamente, mas caso seja necessario retornar o objeto atualizado, devemos fazer da seguinte maneira:
+      
+   // O primeiro parametro sera o "id", o segundo sera o valor que queremos alterar, e o terceiro e se queremos que a funcao retorne o valor atualizado
+   const product = await ProductsModel.findOneAndUpdate({ _id: id }, req.body, { new: true })
+
+   res.send({
+      message: 'Success',
+      product,
+   })
+}
 
 module.exports = {
    get,
    post,
+   put,
 }
